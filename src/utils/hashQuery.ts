a013ec9f6a85fb01ed381df56a04511afcf99b79
Khadija Zehra <162561139+khadija-nordstone@users.noreply.github.com>
2025-10-28 22:02:00 +0500

    update

--- src/utils/hashQuery.ts
+++
@@ -1,8 +0,0 @@
-import crypto from "crypto";
-
-export function hashQuery(query: string) {
-  return crypto
-    .createHash("sha256")
-    .update(query.trim().toLowerCase())
-    .digest("hex");
-}
