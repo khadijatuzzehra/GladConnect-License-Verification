668bc38c9555f1a6e2bc7e2899494197c6fa61cd
Khadija Zehra <162561139+khadija-nordstone@users.noreply.github.com>
2025-10-29 11:25:59 +0500

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import route from "./routes/index.routes";
+import cors from "cors";
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/api", route);
 
 app.get("/", (req, res) => {
